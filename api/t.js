974840f3196f3191447f701b995159192359d34e
dwk001 <dwk001@example.com>
2025-10-02 17:18:04 -0400

    Fix: Pass both native and web URLs to Apple Music and YouTube Music buttons

--- api/t.js
+++ api/t.js
@@ -168,9 +168,10 @@ module.exports = async (req, res) => {
                     </button>
                   `;
                  } else {
+                   const escapedDeepLink = (provider.deepLinkHint || '').replace(/'/g, "\\'");
                    const escapedWebUrl = (provider.webUrl || '').replace(/'/g, "\\'");
                    return `
-                     <button class="provider-btn ${provider.name}" onclick="openMusicApp('${escapedWebUrl}')">
+                     <button class="provider-btn ${provider.name}" onclick="openMusicApp('${escapedDeepLink}', '${escapedWebUrl}')">
                        ${
                          provider.name === 'apple_music' ? `<svg width="20" height="20" viewBox="0 0 24 24" fill="#000000"><path d="M18.71 19.5c-.83 1.24-1.71 2.45-3.05 2.47-1.34.03-1.77-.79-3.29-.79-1.53 0-2 .77-3.27.82-1.31.05-2.3-1.32-3.14-2.53C4.25 17 2.94 12.45 4.7 9.39c.87-1.52 2.43-2.48 4.12-2.51 1.28-.02 2.5.87 3.29.87.78 0 2.26-1.07 3.81-.91.65.03 2.47.26 3.64 1.98-.09.06-2.17 1.28-2.15 3.81.03 3.02 2.65 4.03 2.68 4.04-.03.07-.42 1.44-1.38 2.83M13 3.5c.73-.83 1.94-1.46 2.94-1.5.13 1.17-.34 2.35-1.04 3.19-.69.85-1.83 1.51-2.95 1.42-.15-1.15.41-2.35 1.05-3.11z"/></svg>` :
                          provider.name === 'youtube_music' ? `<svg width="20" height="20" viewBox="0 0 24 24" fill="#FF0000"><path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z"/></svg>` : ''
